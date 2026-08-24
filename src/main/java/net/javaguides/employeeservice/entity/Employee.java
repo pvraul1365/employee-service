51bf0006b63eeee902cf80964bd794892d7a8b55
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-08-24 10:30:01 +0200

    163. EmployeeService - Create Employee JPA Entity and Spring Data JPA Repository

---
+++ src/main/java/net/javaguides/employeeservice/entity/Employee.java
@@ -0,0 +1,41 @@
+package net.javaguides.employeeservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Employee
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - pvraul
+ * @version 24/08/2026 - 10:24
+ * @since 1.25
+ */
+@Entity
+@Table(name = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+}
